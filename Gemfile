3b3a15aee048950f7a26c92a3c80499f4759894e
Jesse Squires <jesse@jessesquires.com>
2022-04-28 11:48:33 -0700

    simulate acutal xcconfig / app version setup

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'cocoapods', '~> 1.0'
+gem 'danger', '~> 8.0'
+gem 'fastlane', '~> 2.0'
+gem 'xcodeproj'
+gem 'xcpretty'
+gem 'synx'
+gem 'kintsugi'
--- scripts/app_version.rb
+++ scripts/app_version.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
 
-puts "1.2.3"
+# prints current app version from xcconfig
+# expects to be run from project root directory
+# used in GitHub workflow to create new release
+
+require 'rubygems'
+require 'bundler/setup'
+require 'Xcodeproj'
+
+config = Xcodeproj::Config.new('Sources/App.xcconfig')
+version = config.attributes['MARKETING_VERSION']
+
+puts "#{version}"
