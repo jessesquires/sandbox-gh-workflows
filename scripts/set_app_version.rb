3b379549c0906b25096900805a4afad7908eb925
Jesse Squires <jesse@jessesquires.com>
2022-05-28 11:04:47 -0400

    add initial hotfix release workflow

--- scripts/set_app_version.rb
+++ scripts/set_app_version.rb
@@ -7,6 +7,6 @@ require 'bundler/setup'
 
 base_dir = File.expand_path(".")
 path = "#{base_dir}/Sources/App.xcconfig"
-contents = "MARKETING_VERSION = 2.0.0\n"
+contents = "MARKETING_VERSION = 1.2.4\n"
 
 File.open(path, "w"){ |f| f.write contents }
